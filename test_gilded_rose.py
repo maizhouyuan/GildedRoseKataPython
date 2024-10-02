535cf82172479530fe6c8c1664ebac08df54699d
maizhouyuan <yuan.mai@northeastern.edu>
2024-10-01 20:23:17 -0700

    Lab4 - Update 3 failing test cases

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -9,7 +9,25 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("foo", 0, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("fixme", items[0].name)
+    
+    def test_quality_never_exceeds_50(self):
+        items = [Item("Aged Brie", 2, 49)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(100, items[0].quality)    # Should be 50
+
+    def test_conjured_items_degrade_twice_as_fast(self):
+        items = [Item("Conjured Mana Cake", 3, 6)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].quality)    # Should be 4
+
+    def test_backstage_passes_quality_drops_to_zero_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(20, items[0].quality)    # Should be 0
 
 
 if __name__ == '__main__':
