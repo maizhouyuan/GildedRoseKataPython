a8dc9ffe4a69d34994fce84364cbabf78461531f
maizhouyuan <yuan.mai@northeastern.edu>
2024-10-08 19:05:09 -0700

    Lab5 - Implement Strategy Pattern to the code

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
 
 
 class Item:
-    """ DO NOT CHANGE THIS CLASS!!!"""
+    """DO NOT CHANGE THIS CLASS!!!"""
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
@@ -12,38 +13,77 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class UpdateStrategy(ABC):
+    @abstractmethod
+    def update_quality(self, item):
+        pass
 
+
+class NormalItemStrategy(UpdateStrategy):
+    def update_quality(self, item):
+        item.sell_in -= 1
+        if item.quality > 0:
+            item.quality -= 1
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality -= 1
+
+
+class AgedBrieStrategy(UpdateStrategy):
+    def update_quality(self, item):
+        item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 0 and item.quality < 50:
+                item.quality += 1
+
+
+class BackstagePassStrategy(UpdateStrategy):
+    def update_quality(self, item):
+        item.sell_in -= 1
+        if item.quality < 50:
+            item.quality += 1
+            if item.sell_in < 11 and item.quality < 50:
+                item.quality += 1
+            if item.sell_in < 6 and item.quality < 50:
+                item.quality += 1
+        if item.sell_in < 0:
+            item.quality = 0
+
+
+class SulfurasStrategy(UpdateStrategy):
+    def update_quality(self, item):
+        pass  # Sulfuras never changes
+
+
+class ConjuredItemStrategy(UpdateStrategy):
+    def update_quality(self, item):
+        item.sell_in -= 1
+        if item.quality > 0:
+            item.quality -= 2
+            if item.sell_in < 0 and item.quality > 0:
+                item.quality -= 2
+        item.quality = max(0, item.quality)  # Ensure quality doesn't go below 0
+
+
+class GildedRose:
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
+        self.strategies = {
+            "Aged Brie": AgedBrieStrategy(),
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePassStrategy(),
+            "Sulfuras, Hand of Ragnaros": SulfurasStrategy(),
+            "Conjured": ConjuredItemStrategy()
+        }
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name.startswith("Conjured"):
+                strategy = self.strategies["Conjured"]
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                strategy = self.strategies.get(item.name, NormalItemStrategy())
+            strategy.update_quality(item)
+            self._update_quality_bounds(item)
+
+    def _update_quality_bounds(self, item):
+        item.quality = max(0, min(item.quality, 50))
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -15,19 +15,19 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("Aged Brie", 2, 49)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(100, items[0].quality)    # Should be 50
+        self.assertEqual(50, items[0].quality)    # Should be 50
 
     def test_conjured_items_degrade_twice_as_fast(self):
         items = [Item("Conjured Mana Cake", 3, 6)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(10, items[0].quality)    # Should be 4
+        self.assertEqual(4, items[0].quality)    # Should be 4
 
     def test_backstage_passes_quality_drops_to_zero_after_concert(self):
         items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEqual(20, items[0].quality)    # Should be 0
+        self.assertEqual(0, items[0].quality)    # Should be 0
 
 
 if __name__ == '__main__':
